3dd95b21aec1f48f7598cc2a2a0ac0171933f503
ceej <ceejjay73@gmail.com>
2025-05-10 20:55:03 +0800

    feat (cardetails.php) added 3 more cars and their details

---
+++ page/cardetails.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Car Details</title>
+    <link href="assets/page/assets/css/styles.css">
+</head>
+<body>
+    <a href="index.php">← Back to list</a>
+    
+    <?php
+    
+    $carDetails = [
+        1 => ["Toyota", "Camry", 2020, "Silver", "25000 miles"],
+        2 => ["Honda", "Civic", 2021, "Blue", "15000 miles"],
+        3 => ["Ford", "Focus", 2019, "Red", "35000 miles"],
+        4 => ["Acura", "NSX", 2000],
+        5 => ["Toyota", "Supra", 2020],
+        6 => ["Mclaren", "F1", 2000]
+    ];
+    
+    if (isset($_GET['id']) && isset($carDetails[$_GET['id']])) {
+        $id = $_GET['id'];
+        $car = $carDetails[$id];
+        
+        echo '<div class="details">';
+        echo '<h2>' . $car[0] . ' ' . $car[1] . '</h2>';
+        echo '<p>Year: ' . $car[2] . '</p>';
+        echo '<p>Color: ' . $car[3] . '</p>';
+        echo '<p>Mileage: ' . $car[4] . '</p>';
+        echo '</div>';
+    } else {
+        echo '<p>Car not found.</p>';
+    }
+    ?>
+</body>
+</html>
