0ccdd2a065caa4064fb29ab28cd2f3df3627b460
ceej <ceejjay73@gmail.com>
2025-05-10 21:53:27 +0800

    refactor (cardetails.php) changed the structure of the if statement

--- page/cardetails.php
+++ page/cardetails.php
@@ -2,13 +2,12 @@
 <html>
 <head>
     <title>Car Details</title>
-    <link href="assets/css/styles.css" rel="stylesheet">
+    <link href="assets/css/styles2.css" rel="stylesheet">
 </head>
 <body>
     <a href="../index.php" class="back-link">← Back to list</a>
     
     <?php
-    
     $carDetails = [
         1 => ["Toyota", "Camry", 2020, "Silver", "2500 miles"],
         2 => ["Honda", "Civic", 2021, "Blue", "6000 miles"],
@@ -18,17 +17,20 @@
         6 => ["Mclaren", "F1", 2000, "Orange", "0 miles"]
     ];
     
-    if (isset($_GET['id']) && isset($carDetails[$_GET['id']])) {
-        $id = $_GET['id'];
+    if (isset($_GET['id'])) {
+        $id = (int) $_GET['id'];
+        if(isset($carDetails[$id])) {
         $car = $carDetails[$id];
-        
         echo '<div class="details">';
         echo '<h2>' . $car[0] . ' ' . $car[1] . '</h2>';
         echo '<p>Year: ' . $car[2] . '</p>';
         echo '<p>Color: ' . $car[3] . '</p>';
         echo '<p>Mileage: ' . $car[4] . '</p>';
-        echo '</div>';
-    } else {
+        echo '</div>';  
+        } else {
+        echo '<p>Car not found.</p>';
+    } 
+}else {
         echo '<p>Car not found.</p>';
     }
     ?>
